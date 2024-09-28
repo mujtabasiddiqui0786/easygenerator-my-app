8759856a97242c981cb15cfe4d93535c32cbfa6b
mjLootmogul <mujtaba.siddiqui@nextgentm.com>
2024-09-29 00:33:09 +0530

    env update

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import API_BASE_URL from './apiConfig';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_UR,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
 });
 
 apiClient.interceptors.request.use((config) => {
