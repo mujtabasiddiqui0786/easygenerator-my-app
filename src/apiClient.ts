2f8b6063483e96094bc62c91075d040d2bf317bb
mjLootmogul <mujtaba.siddiqui@nextgentm.com>
2024-09-29 12:00:19 +0530

    debug be url

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,7 +1,7 @@
 // src/apiClient.ts
 import axios from 'axios';
 import API_BASE_URL from './apiConfig';
-
+console.log('Backend URL:', process.env.REACT_APP_BACKEND_URL);
 const apiClient = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
 });
